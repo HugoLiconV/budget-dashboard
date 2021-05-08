1d4b9ccbf58bd3c28260e0e31638ec3ebbba64d9
Hugo Licon <hugo_val28@hotmail.com>
2021-05-07 20:22:56 -0600

    refactor styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Account from "./components/account";
 const Header = styled.h1`
   padding: 32px;
   text-align: center;
-  color: var(--color);
+  color: var(--font-color);
 `;
 
 function App() {

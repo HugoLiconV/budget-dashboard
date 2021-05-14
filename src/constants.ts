27f44c5b615840072536df6d5521314118fff258
Hugo Licon <hugo_val28@hotmail.com>
2021-05-13 20:39:10 -0600

    fix api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://google-sheets-budget-dashboard.herokuapp.com/dashboard' 
+  ? 'https://google-sheets-budget-dashboard.herokuapp.com' 
   : 'http://192.168.101.5:8080'

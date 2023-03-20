ade8d860d0bac1f05cd62cc4b404121e8fd6d040
Hugo Licon <hugo_val28@hotmail.com>
2023-03-20 14:41:50 -0600

    fix bottom sheet bg color

--- src/atomic/bottom-sheet.tsx
+++ src/atomic/bottom-sheet.tsx
@@ -3,8 +3,8 @@ import { BottomSheet as BottomSheetBase } from "react-spring-bottom-sheet";
 import styled from "styled-components";
 import "react-spring-bottom-sheet/dist/style.css";
 
-const Container = styled.div`
-  background-color: rgba(255, 255, 255, 1);
+const StyledBottomSheet = styled(BottomSheetBase)`
+  --rsbs-bg: #fff;
 `;
 
 type Props = {
@@ -23,20 +23,17 @@ const BottomSheet: React.FC<Props> = ({
   footer,
 }: Props) => {
   return (
-    <Container>
-      <BottomSheetBase
-        open={isOpen}
-        onDismiss={onDismiss}
-        defaultSnap={({ snapPoints, lastSnap }) =>
-          lastSnap ?? Math.max(...snapPoints)
-        }
-        snapPoints={({ maxHeight }) => [maxHeight - maxHeight / 5, maxHeight]}
-        header={header}
-        footer={footer}
-      >
-        {children}
-      </BottomSheetBase>
-    </Container>
+    <StyledBottomSheet
+      style={{ background: "#0095ff" }}
+      open={isOpen}
+      onDismiss={onDismiss}
+      defaultSnap={({ snapPoints }) => Math.max(...snapPoints)}
+      snapPoints={({ maxHeight }) => [maxHeight - maxHeight / 5, maxHeight]}
+      header={header}
+      footer={footer}
+    >
+      {children}
+    </StyledBottomSheet>
   );
 };
 
